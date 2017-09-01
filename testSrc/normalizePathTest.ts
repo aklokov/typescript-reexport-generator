0208d32e65dd85c2cf93cf30be88ed1c1c6fec6c
repinvv <repin.vladv@gmail.com>
2017-09-01 13:58:22 +0300

    couple more tests

--- testSrc/normalizePathTest.ts
+++ testSrc/normalizePathTest.ts
@@ -2,9 +2,31 @@ import { normalizePath } from '../src/generateReexports';
 import { expect } from 'chai';
 
 describe('normalizePath', function (): void {
-  it('should replace slashes', async function (): Promise<void> {
+  it('should replace back slashes', async function (): Promise<void> {
     // arrange
     const src = '.\\a\\b';
+
+    // act
+    const result = normalizePath(src);
+
+    // assert
+    expect(result).to.be.equal('./a/b');
+  });
+
+  it('should not touch right slashes', async function (): Promise<void> {
+    // arrange
+    const src = './a/b';
+
+    // act
+    const result = normalizePath(src);
+
+    // assert
+    expect(result).to.be.equal('./a/b');
+  });
+
+  it('should not trim trailing slash', async function (): Promise<void> {
+    // arrange
+    const src = './a/b/';
 
     // act
     const result = normalizePath(src);
